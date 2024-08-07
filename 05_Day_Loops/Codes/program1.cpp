2e77a1d6b76991aae99111490811b4b5302084b7
shubhamdg45 <shubhamg2023@gmail.com>
2024-08-07 21:55:23 +0530

    Print All Alphabet

---
+++ 05_Day_Loops/Codes/program1.cpp
@@ -0,0 +1,18 @@
+
+//print all alphabet
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+
+
+
+    char name;
+
+    for(name=97;name<=122;name++){  // from ascii value
+        cout<<name<<" ";
+    }
+
+    return 0;
+}
