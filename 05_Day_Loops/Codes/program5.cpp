4a4f3a9406b2a2381c39d95ec9850bd230786407
shubhamdg45 <shubhamg2023@gmail.com>
2024-08-07 21:57:41 +0530

    Calculate Power Of Number

---
+++ 05_Day_Loops/Codes/program5.cpp
@@ -0,0 +1,29 @@
+// calculate power
+//
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	int n,pow,num;
+
+	cout<<"Enter the number:";
+	cin>>n;
+
+	cout<<"enter the power : ";
+	cin>>pow;
+
+	num =n;
+	for(int i=1;i<pow;i++){
+
+		num=num * n;
+	}
+
+	cout<<num;
+
+
+
+
+	return 0;
+}
