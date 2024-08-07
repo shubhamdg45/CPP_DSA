a0a817d7cf7c594a555ba66c020ddc0179ef3953
shubhamdg45 <shubhamg2023@gmail.com>
2024-08-07 21:56:59 +0530

    Multiplication Of Number(Table)

---
+++ 05_Day_Loops/Codes/program4.cpp
@@ -0,0 +1,26 @@
+// Multiplication of Number
+//
+//
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	int n;
+	cout<<"Enter a number:";
+
+	cin>>n;
+
+	for(int i=1;i<=10;i++){
+
+		cout<< n * i << endl;
+
+
+	}
+
+
+
+
+	return 0;
+}
