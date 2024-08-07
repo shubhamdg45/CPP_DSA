c40443533b3bc4dd1c21465510304dbb6292d827
shubhamdg45 <shubhamg2023@gmail.com>
2024-08-07 21:56:23 +0530

    Print Ap Sequence

---
+++ 05_Day_Loops/Codes/program3.cpp
@@ -0,0 +1,23 @@
+// form ap 1,4,7,10....
+//
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+	int n;
+
+	cin>>n;
+
+	for(int i=1;i<=n;i+=3){
+		
+		cout<<i;
+	}
+
+
+
+	return 0;
+}
+
