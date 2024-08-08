3e0cccd8f766fdb7b5ebd7146aae125c18cc1a88
shubhamdg45 <shubhamg2023@gmail.com>
2024-08-08 23:52:57 +0530

    Print Pattern (a-e)

---
+++ 06_Day_Pattern_Printing/Codes/program6.cpp
@@ -0,0 +1,28 @@
+//a b c d e
+//a b c d e
+//a b c d e
+//a b c d e
+//a b c d e
+//
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	for(int i=1;i<=5;i++){
+		char name ='a';
+		for(int j=1;j<=5;j++)
+		{
+
+
+			cout<<name;
+			name++;
+		}
+		cout<<endl;
+	}
+
+
+	return 0;
+}
+
