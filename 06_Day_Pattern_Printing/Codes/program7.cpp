23067efa570cab87fcc2d95363e67733923bcc3b
shubhamdg45 <shubhamg2023@gmail.com>
2024-08-09 23:15:19 +0530

    Print Pattern(1-16)

---
+++ 06_Day_Pattern_Printing/Codes/program7.cpp
@@ -0,0 +1,28 @@
+/*
+   Print Pattern: 
+
+   1 2 3 4
+   5 6 7 8 
+   9 10 11 12
+   13 14 15 16
+   
+*/
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	int count =1;
+	for(int i=1;i<5;i++){
+		//int count=1;
+		for(int j=1;j<5;j++){
+			//int count =1;
+		         cout<<count++<<" ";
+		}
+		cout<< endl;
+	}
+
+
+	return 0;
+}
