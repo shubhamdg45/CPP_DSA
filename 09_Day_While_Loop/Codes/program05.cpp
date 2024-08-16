e8b0542772c6b4c9538546924ce43eb4a7252a7c
shubhamdg45 <shubhamg2023@gmail.com>
2024-08-16 08:02:43 +0530

    Reverse Number

---
+++ 09_Day_While_Loop/Codes/program05.cpp
@@ -0,0 +1,22 @@
+//reverse number
+#include<iostream>
+using namespace std;
+int main1(){
+    int n;
+    cin>>n;
+    int ld;
+    while(n>0){
+        ld=n%10;
+        n=n/10;
+        
+cout<<ld;
+    }
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    while(n>0){
+        
+    }
+}
